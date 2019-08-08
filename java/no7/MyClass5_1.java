878f5b1a6af8c84118e1d5faaf83e7409b1c7934
riha-git <d09048@shibaura-it.ac.jp>
2019-08-09 00:39:47 +0900

    add

---
+++ java/no7/MyClass5_1.java
@@ -0,0 +1,15 @@
+package Pac3_1;
+
+public class MyClass5_1
+{
+	// 他のパッケージからアクセス可
+	// フィールド変数
+	public int a = 3;
+
+	// メソッド
+	public void method(int x, int y)
+	{
+		int sum = x + y;
+		System.out.println(sum);
+	}
+}
