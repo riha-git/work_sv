6e49f080c638f3365459322cc5438df23f2c7f28
riha-git <d09048@shibaura-it.ac.jp>
2019-08-30 20:42:04 +0900

    add

---
+++ 2_practiceOfJava/chapter_02/MaterialManager.java
@@ -0,0 +1,5 @@
+public class MaterialManager
+{
+        int mouse;
+        int keyboard;
+}
---
+++ 2_practiceOfJava/chapter_02/Mondai1.java
@@ -0,0 +1,12 @@
+public class Mondai1
+{
+        public static void main(String[] args)
+        {
+                MaterialManager stock = new MaterialManager();
+                stock.mouse = 2;
+                stock.keyboard = 4;
+                System.out.println("マウスの数は" + stock.mouse + "個です。"); 
+                System.out.println("キーボードの数は" + stock.mouse + "個です。"); 
+        }
+}
+
