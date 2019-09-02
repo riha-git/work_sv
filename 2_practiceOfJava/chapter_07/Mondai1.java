e2c5c8a1c1ae04e651099dfa40bb0b0eb7e847f3
riha-git <d09048@shibaura-it.ac.jp>
2019-09-02 13:54:21 +0900

    add

--- 2_practiceOfJava/chapter_07/Mondai1.java
+++ 2_practiceOfJava/chapter_07/Mondai1.java
@@ -5,5 +5,26 @@ import java.util.List;
 
 public class Mondai1
 {
+        public static void main(String[] args)
+        {
+                List<String> dinners = new ArrayList<String>();
+
+                dinners.add("ハンバーグ");
+                dinners.add("オムライス");
+                dinners.add("サラダ");
+                dinners.add("カレー");
+                dinners.add("ステーキ");
+
+                for(int i = 0; i < dinners.size(); i++)
+                {
+                        System.out.print(dinners.get(i));
+                        if(i != dinners.size() -1)
+                        {
+                                System.out.print(",");
+                        }
+
+                        System.out.println();
+                }
+        }
 }
 
